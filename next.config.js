69ee5bda29fbb1a2835cf50dafcc99b10ae49e3e
Younjin9 <qazwsx12098@naver.com>
2026-02-19 16:45:12 +0900

    fix: add COOP headers for Google Login popups

--- next.config.js
+++ next.config.js
@@ -13,6 +13,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin-allow-popups" },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
